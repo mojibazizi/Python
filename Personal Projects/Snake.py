829519171962bd236dee7ae3fbf7f4131002a3b0
Mojib Azizi <mojibazizi@outlook.com>
2022-06-21 21:15:00 -0600

    Grass and ScoreBoard implemented.

--- Personal Projects/Snake.py
+++ Personal Projects/Snake.py
@@ -1,3 +1,4 @@
+from tkinter import CENTER
 import pygame, sys, random
 from pygame.math import Vector2
 
@@ -118,6 +119,7 @@ class Snake:
 
 
 class Main:
+    
     def __init__(self):
         self.snake = Snake()
         self.fruit = Fruit() 
@@ -128,10 +130,11 @@ class Main:
             self.check_fail()
     
     def draw_elements(self):
+        self.draw_grass()
         self.fruit.draw_fruit()
         self.snake.draw_snake()
-        self.draw_grass()
-
+        self.draw_score()
+        
     def check_collision(self):
         if self.fruit.pos == self.snake.body[0]:
             print("snack")
@@ -146,20 +149,32 @@ class Main:
         for block in self.snake.body[1:]:
             if block == self.snake.body[0]: # check if snake hits itself
                 self.game_over()        
-
+    
     def draw_grass(self):
         grass_color = (167,209,61)
-        for col in range(cell_number):
-            if col % 2 == 0:
-                grass_rect = pygame.Rect(col * cell_size,0,cell_size,cell_size)
-                pygame.draw.rect(screen, grass_color, grass_rect)   
+        for row in range(cell_number):
+            if row % 2 == 0:
+                for col in range(cell_number):
+                    if col % 2 == 0:
+                        grass_rect = pygame.Rect(col * cell_size,row * cell_size,cell_size,cell_size)
+                        pygame.draw.rect(screen, grass_color, grass_rect)   
+        else:
+            if col % 2 != 0:
+                        grass_rect = pygame.Rect(col * cell_size,row * cell_size,cell_size,cell_size)
+                        pygame.draw.rect(screen, grass_color, grass_rect)  
     
     def game_over(self):
         pygame.QUIT
         sys.exit()
-
         
-
+    def draw_score(self):
+        # The scoreboard
+        score_text = str(len(self.snake.body) - 3)
+        score_surface = game_font.render(score_text,True,(56,74,12))
+        score_x = int(cell_size * cell_number - 60)
+        score_y = int(cell_size * cell_number - 40)
+        score_rect = score_surface.get_rect(center = (score_x,score_y))
+        screen.blit(score_surface,score_rect)
 
 # game interface
 pygame.init()
@@ -169,6 +184,7 @@ screen = pygame.display.set_mode((cell_number * cell_size,cell_number * cell_siz
 clock = pygame.time.Clock()
 clock = pygame.time.Clock()
 apple = pygame.image.load('Personal Projects/Graphics/apple.png').convert_alpha()
+game_font = pygame.font.Font(None, 27)
 
 main_game = Main()
 
@@ -198,7 +214,7 @@ while True:                                                            # Main ga
                 if main_game.snake.direction.x != -1:
                     main_game.snake.direction = Vector2(1,0)
 
-    screen.fill((175,0,0))
+    screen.fill((135,210,45))
     main_game.draw_elements()
     pygame.display.update()
     clock.tick(60)
